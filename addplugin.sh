81862fcbb882400bb07abbb0a7d563bda2b1a9be
kensaku komatsu <kensaku.komatsu@gmail.com>
2016-05-27 20:47:11 +0000

    update addplugin.sh

--- addplugin.sh
+++ addplugin.sh
@@ -4,5 +4,8 @@ JG_PATH=../janus-gateway
 
 cp configure.ac ${JG_PATH}/
 cp Makefile.am ${JG_PATH}/
-ln -s conf/janus.plugin.skywayiot.cfg.sample ${JG_PATH}/conf
-ln -s plugins/janus_skywayiot.c ${JG_PATH}/plugins
+
+cd ${JG_PATH}/conf
+ln -s ../../janus-skywayiot-plugin/conf/janus.plugin.skywayiot.cfg.sample .
+cd ../plugins
+ln -s ../../janus-skywayiot-plugin/plugins/janus_skywayiot.c .
